173acb17fd617603eebd77b9870eb04f0373ee91
Vyshnavi <97934908+efyvyshnavi@users.noreply.github.com>
2022-03-23 06:20:49 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,9 @@
+public class Arithmetic{
+public static void main(String args[]){
+int a=20;
+int b=5;
+ System.out.println("a+b="+(a+b));
+ System.out.println("a-b="+(a-b));
+ System.out.println("a*b="+(a*b));
+ }
+ }
---
+++ addition.java
@@ -0,0 +1,7 @@
+public class Addition{
+public static void main(String[]args){
+  int first=10;
+  int second=20;
+  System.out.println(first+second);
+  }
+ }
